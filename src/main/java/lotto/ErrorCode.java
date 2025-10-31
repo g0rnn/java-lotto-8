576f2dc5b4e86eb17d719c4b665bba283e6a9d64
gyunho <gyunh01@pukyong.ac.kr>
2025-10-31 16:13:55 +0900

    refactor(Money): long타입으로 변경한다

--- src/main/java/lotto/ErrorCode.java
+++ src/main/java/lotto/ErrorCode.java
@@ -5,8 +5,8 @@ public enum ErrorCode {
     INVALID_LOTTO_SIZE("로또 번호는 6개여야 합니다."),
     DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
     LOTTO_NUMBER_OUT_OF_RANGE("로또 번호는 1 이상 45 이하여야 합니다."),
-    POSITIVE_AMOUNT_REQUIRED("돈은 0보다 커야합니다."),
-    INVALID_MONEY_UNIT("돈은 1000원 단위여야 합니다.")
+    NEGATIVE_AMOUNT_NOT_ALLOWED("돈은 음수일 수 없습니다."),
+    INVALID_MONEY_UNIT("돈은 1000원 단위여야 합니다."),
     ;
 
     private static final String PREFIX = "[ERROR] ";
--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -1,16 +1,16 @@
 package lotto;
 
 import static lotto.ErrorCode.INVALID_MONEY_UNIT;
-import static lotto.ErrorCode.POSITIVE_AMOUNT_REQUIRED;
+import static lotto.ErrorCode.NEGATIVE_AMOUNT_NOT_ALLOWED;
 
-public record Money(int amount) {
+public record Money(long amount) {
 
     public static final int UNIT = 1000;
-    private static final int MINIMUN_MONEY_AMOUNT = 1;
+    private static final int MINIMUM_MONEY_AMOUNT = 0;
 
     public Money {
-        if (amount < MINIMUN_MONEY_AMOUNT) {
-            throw new IllegalArgumentException(POSITIVE_AMOUNT_REQUIRED.getMessage());
+        if (amount < MINIMUM_MONEY_AMOUNT) {
+            throw new IllegalArgumentException(NEGATIVE_AMOUNT_NOT_ALLOWED.getMessage());
         }
 
         if (amount % UNIT != 0) {
