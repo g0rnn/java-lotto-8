ded546d4d93b52c41e735956bcd58a05fc20b960
gyunho <gyunh01@pukyong.ac.kr>
2025-11-01 13:49:33 +0900

    feat(LottoController): 입출력과 로또 도메인을 다루는 객체를 구현한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,65 +1,10 @@
 package lotto;
 
-import java.util.List;
-import java.util.function.Supplier;
 import lotto.console.ConsoleView;
-import lotto.purchase.Lotto;
-import lotto.purchase.LottoFactory;
-import lotto.purchase.LottoNumber;
-import lotto.purchase.Money;
-import lotto.purchase.WinningLotto;
-import lotto.statistics.Aggregator;
-import lotto.statistics.LottoReport;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleView consoleView = new ConsoleView();
-
-        Money money = retryOnException(() -> {
-            long amount = consoleView.readAmount();
-            return new Money(amount);
-        });
-
-        List<Lotto> lottos = LottoFactory.createLottos(money);
-
-        int size = lottos.size();
-        String publishedLotto = getPublishedLotto(lottos);
-        consoleView.printLottos(size, publishedLotto);
-
-        Lotto winning = retryOnException(() -> {
-            List<Integer> numbers = consoleView.readWinningNumbers();
-            return Lotto.of(numbers);
-        });
-
-        WinningLotto winningLotto = retryOnException(() -> {
-            Integer bonus = consoleView.readBonusNumber();
-            LottoNumber bonusNumber = new LottoNumber(bonus);
-            return LottoFactory.createWinningLotto(winning, bonusNumber);
-        });
-
-        Aggregator aggregator = new Aggregator(winningLotto);
-        LottoReport report = aggregator.aggregate(lottos, money);
-        consoleView.printReport(report.toString());
-    }
-
-    private static String getPublishedLotto(List<Lotto> lottos) {
-        StringBuilder builder = new StringBuilder();
-        for (Lotto lotto : lottos) {
-            builder.append(lotto.toString());
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
-    private static <T> T retryOnException(Supplier<T> supplier) {
-        while (true) {
-            try {
-                return supplier.get();
-            } catch (NumberFormatException e) {
-                System.out.println("[ERROR] " + e.getMessage());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        LottoController controller = new LottoController(new ConsoleView());
+        controller.run();
     }
 }
---
+++ src/main/java/lotto/LottoController.java
@@ -0,0 +1,83 @@
+package lotto;
+
+import java.util.List;
+import java.util.function.Supplier;
+import lotto.console.ConsoleView;
+import lotto.purchase.Lotto;
+import lotto.purchase.LottoFactory;
+import lotto.purchase.LottoNumber;
+import lotto.purchase.Money;
+import lotto.purchase.WinningLotto;
+import lotto.statistics.Aggregator;
+import lotto.statistics.LottoReport;
+
+public class LottoController {
+
+    private final ConsoleView consoleView;
+
+    public LottoController(ConsoleView consoleView) {
+        this.consoleView = consoleView;
+    }
+
+    public void run() {
+        Money money = getMoney();
+        List<Lotto> lottos = LottoFactory.createLottos(money);
+        printPublishedLotto(lottos);
+
+        WinningLotto winningLotto = getWinningLotto();
+        printStatistics(winningLotto, lottos, money);
+    }
+
+    private Money getMoney() {
+        return retryOnException(() -> {
+            long amount = consoleView.readAmount();
+            return new Money(amount);
+        });
+    }
+
+    private WinningLotto getWinningLotto() {
+        Lotto winning = retryOnException(() -> {
+            List<Integer> numbers = consoleView.readWinningNumbers();
+            return Lotto.of(numbers);
+        });
+
+        return retryOnException(() -> {
+            Integer bonus = consoleView.readBonusNumber();
+            LottoNumber bonusNumber = new LottoNumber(bonus);
+            return LottoFactory.createWinningLotto(winning, bonusNumber);
+        });
+    }
+
+    private void printPublishedLotto(List<Lotto> lottos) {
+        int size = lottos.size();
+        String publishedLotto = getPublishedLotto(lottos);
+        consoleView.printLottos(size, publishedLotto);
+    }
+
+    private void printStatistics(WinningLotto winningLotto, List<Lotto> lottos, Money money) {
+        Aggregator aggregator = new Aggregator(winningLotto);
+        LottoReport report = aggregator.aggregate(lottos, money);
+        consoleView.printReport(report.toString());
+    }
+
+    private <T> T retryOnException(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private String getPublishedLotto(List<Lotto> lottos) {
+        StringBuilder builder = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            builder.append(lotto.toString());
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+}
